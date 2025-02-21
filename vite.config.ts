fded2341b59e14fab9c395b57ac0c959b4b2d050
mirasbox2022 <mirasbox2022@mail.ru>
2025-02-21 22:43:56 +0500

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: './', // Указывает, что все пути должны быть относительными
+})
